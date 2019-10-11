ba24172013f41d62d15ccfeb6cbfdcbb72b924b7
Vague <vaguemail369@gmail.com>
2019-10-11 16:48:14 +0100

    Reduce countdown time to 2seconds

--- app/src/main/java/com/makeupproject/android/networth45/MainActivity.kt
+++ app/src/main/java/com/makeupproject/android/networth45/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val countDownTimer = object : CountDownTimer(10000, 1000) {
+        val countDownTimer = object : CountDownTimer(2000, 1000) {
             override fun onTick(millisUntilFinished: Long) {
             }
 
