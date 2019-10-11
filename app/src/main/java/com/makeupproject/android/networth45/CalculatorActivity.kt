7d003ec07bf6d2458bb78b1aa2aa783cd0d5f7f7
Vague <vaguemail369@gmail.com>
2019-10-11 17:15:47 +0100

    Add the fragments to the activity's navigation

--- app/src/main/java/com/makeupproject/android/networth45/CalculatorActivity.kt
+++ app/src/main/java/com/makeupproject/android/networth45/CalculatorActivity.kt
@@ -1,7 +1,12 @@
 package com.makeupproject.android.networth45
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class CalculatorActivity : AppCompatActivity() {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calculator)
+    }
 }
