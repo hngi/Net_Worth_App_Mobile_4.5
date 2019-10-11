fdf9ccfe004f8bde5cc383d3a1694df7a3d943bc
Vague <vaguemail369@gmail.com>
2019-10-11 17:05:31 +0100

    Open the calculator after the user is done with the intro

---
+++ app/src/main/java/com/makeupproject/android/networth45/CalculatorActivity.kt
@@ -0,0 +1,7 @@
+package com.makeupproject.android.networth45
+
+import androidx.appcompat.app.AppCompatActivity
+
+class CalculatorActivity : AppCompatActivity() {
+
+}
--- app/src/main/java/com/makeupproject/android/networth45/FinallyActivity.kt
+++ app/src/main/java/com/makeupproject/android/networth45/FinallyActivity.kt
@@ -1,6 +1,8 @@
 package com.makeupproject.android.networth45
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 class FinallyActivity : AppCompatActivity() {
@@ -8,5 +10,10 @@ class FinallyActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_finally)
+    }
+
+    fun goToCalculator(view: View) {
+        val intent = Intent(this@FinallyActivity, CalculatorActivity::class.java)
+        startActivity(intent)
     }
 }
